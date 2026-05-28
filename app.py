62b095b3753d04483a05d276046365af7ac81350
Ankur Singh <singhankur0604@gmail.com>
2026-05-28 13:13:40 +0530

    Fix Chroma import and pin versions

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
